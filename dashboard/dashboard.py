0fbab578e6830f65e7a8459d2bd40cf65d3e118c
Parth Ugale <parthhugale23@gmail.com>
2026-03-27 18:59:45 +0530

    File added sucesssfully

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
+from datetime import date
 
 def show_dashboard():
 
-    st.header("Dashboard")
+    st.header("📊 Dashboard")
 
     df = pd.read_csv("data.csv")
-    st.dataframe(df)
-    
+
+    st.write(df)
